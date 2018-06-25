2d535c2639519a72e32879b16155718fe0ac562a
LucasALC <lucasandrelescanocruz@gmail.com>
2018-06-25 09:27:22 -0400

    Add project files.

---
+++ WebAppCurriculo/Models/Curriculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurriculo.Models
+{
+    public class Curriculo
+    {
+        //Informações pessoais
+        public int CurriculoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public decimal Altura { get; set; }
+        public string Site { get; set; }
+        public EstadoCivil EstadoCivil { get; set; }
+        public Sexo Sexo { get; set; }
+
+        // Endereço
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+
+        // Contato
+        public string TelefoneFixo { get; set; }
+        public string TelefoneMovel { get; set; }
+
+        //Conhecimentos
+        public bool Html { get; set; }
+        public bool JavaScript { get; set; }
+        public bool Css { get; set; }
+        public string MaisAdicionais { get; set; }
+       
+    }
+}
---
+++ WebAppCurriculo/Models/Sexo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCurriculo.Models
+{
+    public enum Sexo
+    {
+        masculino = 1, 
+        feminino = 2
+    }
+}
